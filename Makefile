959863eaa7c08ca6a02b7de788f551651395fe80
Brian Ketelsen <bketelsen@gmail.com>
2023-08-16 14:19:12 -0400

    S2E1

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+clean:
+	rm -rf docs/*.txt
+	rm -rf docs/*.json
+	rm -rf output/*.*
+
+run:
+	python3 app.py
--- app.py
+++ app.py
@@ -3,6 +3,40 @@ import glob
 import boto3
 import hashlib
 
+# create a metadata structure for the document
+class DocumentMetadata:
+    def __init__(self, name, md5, entities, docdate, mainentity, documenttype,category,containspii):
+        self.name = name
+        self.md5 = md5
+        self.entities = entities
+        self.docdate = docdate
+        self.mainentity = mainentity
+        self.documenttype = documenttype
+        self.category = category
+        self.containspii = containspii
+
+    def __str__(self):
+        return self.name + " " + self.md5 + " " + self.entities + " " + self.docdate + " " + self.mainentity
+    
+
+    def pretty_print(self):
+        pretty_output = """Name={}
+MD5={}
+Entities={}
+DocumentDate={}
+MainEntity={}
+DocumentType={}
+Category={}
+ContainsPII={}
+""".format(self.name, self.md5, self.entities, self.docdate, self.mainentity, self.documenttype, self.category, self.containspii)
+        return pretty_output
+
+
+    def save(self, file):
+        with open(file, 'w') as f:
+            f.write(self.pretty_print())
+        f.close()
+
 # get list of files in directory
 def get_files():
     files = glob.glob("docs/*.pdf")
@@ -29,6 +63,13 @@ def analyze_document(bytes):
     )
     return response
 
+# with boto3, use amazon comprehend to detect entities in the document
+def detect_entities(str):
+    client = boto3.client('comprehend')
+    response = client.detect_entities(Text=str, LanguageCode='en')
+    return response
+
+
 # read the bytes of a file
 def read_file(file):
     with open(file, 'rb') as f:
@@ -38,9 +79,6 @@ def read_file(file):
 def md5_hash(bytes):
     return hashlib.md5(bytes).hexdigest()
 
-
-
-
 def main():
     # iterate through list of files
     files = get_files()
@@ -49,47 +87,55 @@ def main():
     md5_dict = {}
     duplicates = []
 
-    print("your files")
     for x in range (len(files)):
         # print each file name to the console
         print(files[x])
-        # check for json version of the document
-        if os.path.isfile(files[x]+ '.json'):
-            print("json file already exists")
-            with open(files[x]+ '.json', 'r', encoding='utf-8') as f:
-            
-                #result =get_text(f.read())
-                res = f.read()
-                # if the md5 hash of the text is in the dictionary, 
-                # add the file to the duplicate list
-                if md5_hash(res.encode('utf-8')) in md5_dict:
-                    duplicates.append(files[x])
-                else:
-                    # add the md5 hash to the dictionary
-                    md5_dict[md5_hash(res.encode('utf-8'))] = files[x]
-                    #save the result in a text file
-                    with open(files[x]+ '.txt', 'w') as f:
-                        f.write(str(res))
-                    f.close()
-                   
+
+        bytes = read_file(files[x])
+        filehash = md5_hash(bytes)
+        if filehash in md5_dict:
+            duplicates.append(files[x])
         else:
-            bytes = read_file(files[x])
+
+            # copy the file into the output directory
+            # with the md5 hash as the file name
+            os.system("cp "+ files[x] + " output/" + filehash + ".pdf")
+
+            md5_dict[filehash] = files[x]
             response = analyze_document(bytes)
             # write response in a a json file
-            with open(files[x]+ '.json', 'w') as f:
+            with open("output/" + filehash + '.json', 'w') as f:
                 f.write(str(response))
                 f.close()
-                print("json response saved")
                 result = get_text(response)
                 if md5_hash(str(bytes).encode('utf-8')) in md5_dict:
-                    print("DEBUG: Duplicates found")
                     duplicates.append(files[x])
                 else:
                     md5_dict[md5_hash(str(bytes).encode('utf-8'))] = files[x]
-                    #save the result in a text file
-                    print("DEBUG: Duplicates NOT found")
-                    with open(files[x]+ '.txt', 'w') as f:
-                        f.write(str(result))
+                    # this is the list of entities in the document
+                    detectresult = detect_entities(str(result))
+                    with open("output/" + filehash + '.entities.txt', 'w') as f:
+                        f.write(str(detectresult))
+                    f.close()
+
+                    firstdatefound = False
+                    firstdate = ""
+
+                    # iterate through the entities in the list, finding the first date
+                    # with a confidence greater than 99%
+                    for item in detectresult["Entities"]:
+                        if item["Score"] > 0.99 and item["Type"] == "DATE":
+                            firstdatefound = True
+                            firstdate = item["Text"]
+                            break
+
+                    # if a date was found, print it to the console
+                    if firstdatefound:
+                        print("first date: " +firstdate)
+
+                    # create a document metadata object
+                    doc = DocumentMetadata(files[x], filehash, "", firstdate, "","","",False)
+                    doc.save("output/" + filehash + '.metadata.txt')
 
     print("---------------------------------------------")
     print(duplicates)
